7890828f8757424146eccd2b23e19740520c7109
Crisly Domingos_Admin <crislydomingos_admin@haystravel.local>
2019-03-21 11:15:32 +0000

    authotication required to create a project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -24,6 +24,8 @@ class ProjectsController extends Controller
             'description' => 'required'
         ]);
 
+        $attributes['owner_id'] = auth()->id();
+        
         Project::create($attributes);
 
         return redirect('/projects');
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -53,4 +53,12 @@ class ProjectsTest extends TestCase
         $attributes = factory('App\Project')->raw(['description' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
+
+    /** @test */
+    public function a_project_requires_an_owner()
+    {
+        $attributes = factory('App\Project')->raw();
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
 }
