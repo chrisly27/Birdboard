f665bbd1afc7293088239f1ea6aa7b95dacd4cb5
Crisly Domingos <crislydomingos@haystravel.local>
2019-03-21 11:37:26 +0000

    update the database - users table

--- database/migrations/2019_03_21_103516_create_users_table.php
+++ database/migrations/2019_03_21_103516_create_users_table.php
@@ -15,6 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->boolean('email_verified_at');
+            $table->string('password');
+            $table->string('remember_token');
             $table->timestamps();
         });
     }
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -10,12 +10,22 @@ class ProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
+    
+    /** @test */
+    public function only_authenticated_users_can_create_project()
+    {
+        $attributes = factory('App\Project')->raw();
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
     /** @test */
     public function a_user_can_create_a_project()
     {
 
         $this->withoutExceptionHandling();
 
+        
         $attributes = [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph
@@ -43,6 +53,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+        
         $attributes = factory('App\Project')->raw(['title' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
     }
@@ -52,13 +64,5 @@ class ProjectsTest extends TestCase
     {
         $attributes = factory('App\Project')->raw(['description' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
-    }
-
-    /** @test */
-    public function a_project_requires_an_owner()
-    {
-        $attributes = factory('App\Project')->raw();
-
-        $this->post('/projects', $attributes)->assertRedirect('login');
     }
 }
