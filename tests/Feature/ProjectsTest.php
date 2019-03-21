56a5154533ec5a07f7c44b2353bc956aef66e26d
Crisly Domingos_Admin <crislydomingos_admin@haystravel.local>
2019-03-21 12:06:14 +0000

    update the database table 'projects' because the test was failuring as the owners_id was not created properly

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -25,6 +25,8 @@ class ProjectsTest extends TestCase
 
         $this->withoutExceptionHandling();
 
+        $this->actingAs(factory('App\User')->create());
+
         
         $attributes = [
             'title' => $this->faker->sentence,
@@ -54,7 +56,7 @@ class ProjectsTest extends TestCase
     public function a_project_requires_a_title()
     {
         $this->actingAs(factory('App\User')->create());
-        
+
         $attributes = factory('App\Project')->raw(['title' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
     }
@@ -62,7 +64,10 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description' => '']);
+
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
