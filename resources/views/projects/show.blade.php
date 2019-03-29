63d478ed0741f78a734434fe37bf1c89f02a8784
Crisly Domingos <crislydomingos@haystravel.local>
2019-03-29 13:31:26 +0000

    theme switcher

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,101 +1,91 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
-    @section('content')
+@section('content')
+    <header class="flex items-center mb-6 pb-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-muted font-light">
+                <a href="/projects" class="text-muted no-underline hover:underline">My Projects</a>
+                / {{ $project->title }}
+            </p>
 
-        <header class="flex items-center mb-3 py-4">
-            <div class="flex justify-between items-end w-full">
-
-                <p class="text-grey text-sm font-normal">
-                    
-                    <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
-
-                </p>
-
-                <div class="flex items-center">
-                    @foreach ($project->members as $member)
-                        <img
-                            src="{{ gravatar_url($member->email) }}"
-                            alt="{{ $member->name }}'s avatar"
-                            class="rounded-full w-8 mr-2">
-                    @endforeach
-    
+            <div class="flex items-center">
+                @foreach ($project->members as $member)
                     <img
-                        src="{{ gravatar_url($project->owner->email) }}"
-                        alt="{{ $project->owner->name }}'s avatar"
+                        src="{{ gravatar_url($member->email) }}"
+                        alt="{{ $member->name }}'s avatar"
                         class="rounded-full w-8 mr-2">
-    
-                    <a href="{{ $project->path().'/edit' }}" class="button ml-4">Edit Project</a>
-                </div>
+                @endforeach
+
+                <img
+                    src="{{ gravatar_url($project->owner->email) }}"
+                    alt="{{ $project->owner->name }}'s avatar"
+                    class="rounded-full w-8 mr-2">
+
+                <a href="{{ $project->path().'/edit' }}" class="button ml-4">Edit Project</a>
             </div>
+        </div>
+    </header>
 
-        </header>
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-lg text-muted font-light mb-3">Tasks</h2>
 
-        <main>
-            <div class="lg:flex -mx-3">
-                <div class="lg:w-3/4 px-3 mb-6">
-
-                    <div class="mb-8">
-                        <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
-                        {{-- tasks --}}
-                        @foreach ($project->tasks as $task)
-                            <div class="card mb-3">
-
-                                <form action="{{ $task->path() }}" method="POST">
-                                    @method('PATCH')
-                                    
-                                    @csrf
-        
-                                    <div class="flex items-center">
-                                        <input name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-grey line-through' : '' }}">
-                                        <input name="completed" type="checkbox" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
-                                    </div>
-                                </form>
-
-                            </div>
-                        @endforeach
-
+                    {{-- tasks --}}
+                    @foreach ($project->tasks as $task)
                         <div class="card mb-3">
-                            <form action="{{ $project->path() . '/tasks' }}" method="POST">
+                            <form method="POST" action="{{ $task->path() }}">
+                                @method('PATCH')
                                 @csrf
 
-
-                                <input placeholder="Add a New Task" class="w-full" name="body">
+                                <div class="flex items-center">
+                                    <input name="body" value="{{ $task->body }}" class="text-default bg-card w-full {{ $task->completed ? 'line-through text-muted' : '' }}">
+                                    <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
+                                </div>
                             </form>
                         </div>
-                    </div>
+                    @endforeach
 
-                    <div>
-                        <h2 class="text-lg text-grey font-normal mg-3">General Notes</h2>
-
-                        {{-- genral notes --}}
-
-                        <form action="{{ $project->path() }}" method="POST">
+                    <div class="card mb-3">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
                             @csrf
-                            @method('PATCH')
-                            
-                            <textarea   name="notes"
-                                        class="card w-full mb-4"
-                                        style="min-height:200px"
-                                        placeholder="Anything special that you want to make a note of?">
-                                        {{ $project->notes }}
-                            </textarea>
 
-                            <button type="submit" class="button">Save</button>
-                    
-                            @include ('errors')
+                            <input placeholder="Add a new task..." class="text-default bg-card w-full" name="body">
                         </form>
                     </div>
-
                 </div>
 
-                <div class="lg:w-1/4 px-3 lg:py-8">
-                    @include('projects.card')
+                <div>
+                    <h2 class="text-lg text-muted font-light mb-3">General Notes</h2>
 
-                    @include ('projects.activity.card')
+                    {{-- general notes --}}
+                    <form method="POST" action="{{ $project->path() }}">
+                        @csrf
+                        @method('PATCH')
 
-                    @can('manage', $project)
-                        @include('projects.invite')
-                    @endcan
+                        <textarea
+                            name="notes"
+                            class="card text-default w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
+
+                        <button type="submit" class="button">Save</button>
+                    </form>
+
+                    @include ('errors')
+                </div>
             </div>
-        </main>
-    @endsection
+
+            <div class="lg:w-1/4 px-3 lg:py-8">
+                @include ('projects.card')
+                @include ('projects.activity.card')
+
+                @can ('manage', $project)
+                    @include ('projects.invite')
+                @endcan
+            </div>
+        </div>
+    </main>
+@endsection
