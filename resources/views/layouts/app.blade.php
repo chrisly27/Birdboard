c151e9c365b39ab354ec91d093d281c5696440f1
Crisly Domingos <crislydomingos@haystravel.local>
2019-03-29 14:17:16 +0000

    make sure the logout form works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,19 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
@@ -40,9 +35,9 @@
                     </h1>
 
                     <div>
-                        <!-- Right Side Of Navbar -->
+
+                        
                         <div class="flex items-center ml-auto">
-                            <!-- Authentication Links -->
                             @guest
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
 
@@ -52,18 +47,35 @@
                             @else
                                 <theme-switcher></theme-switcher>
 
-                                <a
-                                    class="flex items-center text-default no-underline text-sm"
-                                    href="#" role="button"
-                                    data-toggle="dropdown"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                    v-pre
-                                >
-                                    <img width="35"
-                                         class="rounded-full mr-3"
-                                         src="{{ gravatar_url(auth()->user()->email) }}">
-                                </a>
+                                <span class="nav-item dropdown">
+                                    <a
+                                        class="flex items-center text-default no-underline text-sm"
+                                        href="#" role="button"
+                                        data-toggle="dropdown"
+                                        aria-haspopup="true"
+                                        aria-expanded="false"
+                                        v-pre >
+    
+                                        <img width="35"
+                                             class="rounded-full mr-3"
+                                             src="{{ gravatar_url(auth()->user()->email) }}">
+    
+                                             {{ auth()->user()->name }}
+                                    </a>
+    
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+    
+                                            {{ __('Logout') }}
+                                        </a>
+    
+                                        <form action="{{ route('logout') }}" method="POST" id="logout-form" style="display: none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </span>
                             @endguest
                         </div>
                     </div>
